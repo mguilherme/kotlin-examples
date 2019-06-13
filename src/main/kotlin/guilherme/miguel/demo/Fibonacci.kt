cd45cf195b9690b0b62902d6c1660317b9d38e52
Miguel Guilherme <mguilherme82@gmail.com>
2019-06-13 22:55:26 +0100

    simplify range function

--- src/main/kotlin/guilherme/miguel/demo/Fibonacci.kt
+++ src/main/kotlin/guilherme/miguel/demo/Fibonacci.kt
@@ -21,7 +21,7 @@ object Fibonacci {
      * @param operation the function
      * @return A comma separated String
      */
-    fun range(range: IntRange, operation: (Int) -> Int) = range.map(operation).map(Int::toString).joinToString()
+    fun range(range: IntRange, operation: (Int) -> Int) = range.map(operation).joinToString(transform = Int::toString)
 
 }
 
