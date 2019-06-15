763567dc6f20e74a4057c9c8b6c7d67f4dae9580
Miguel Guilherme <mguilherme82@gmail.com>
2019-06-15 15:00:02 +0100

    add pig latin extension function

--- src/main/kotlin/guilherme/miguel/demo/ListAndString.kt
+++ src/main/kotlin/guilherme/miguel/demo/ListAndString.kt
@@ -6,6 +6,23 @@ package guilherme.miguel.demo
  */
 fun Int.toIntList(): List<Int> = this.toString().map(Character::getNumericValue)
 
+/**
+ * Translates a text to Pig Latin.
+ * @return the given text into Pig Latin
+ */
+fun String.toPigLatin(): String {
+
+    val pigWord: (String) -> String = { "${it.takeLast(it.length - 1)}${it.first().toLowerCase()}ay" }
+
+    return trim()
+        .splitToSequence(' ')
+        .filter { it.isNotEmpty() }
+        .map(pigWord)
+        .joinToString(" ")
+        .capitalize()
+}
+
 fun main() {
     println(2324.toIntList())
+    println("The quick brown fox".toPigLatin())
 }
--- src/test/kotlin/guilherme/miguel/demo/ListAndStringTest.kt
+++ src/test/kotlin/guilherme/miguel/demo/ListAndStringTest.kt
@@ -1,12 +1,17 @@
 package guilherme.miguel.demo
 
 import io.kotlintest.matchers.collections.shouldContainExactly
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class ListAndStringTest : StringSpec({
 
     "Should retrieve a list of its digits" {
         2342.toIntList() shouldContainExactly listOf(2, 3, 4, 2)
+    }
+
+    "Should translate to Pig Latin" {
+        "The quick brown fox".toPigLatin() shouldBe "Hetay uickqay rownbay oxfay"
     }
 
 })
